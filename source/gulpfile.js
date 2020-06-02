a2541a917b9175d913857b6d0690cf2d613a7c58
IvanVitiuk <vitukivan05@gmail.com>
2020-06-02 16:43:47 +0300

    add

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,4 +1,7 @@
-var gulp = require('gulp'); //import gulp
+var gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('html', function(){ //create task to cpy all html
     return gulp.src('src/*.html')
@@ -6,7 +9,12 @@ gulp.task('html', function(){ //create task to cpy all html
 });
 
 gulp.task('css', function(){
-    return gulp.src('src/css/*.css')
+    return gulp.src('src/css/**/*.css')
+    .pipe(autoprefixer({
+        browsers: ['cover 99.5%'],
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('build/css'));
 });
 
@@ -15,8 +23,25 @@ gulp.task('img', function(){
     .pipe(gulp.dest('build/img'));
 });
 
+gulp.task('watch', function(){
+    gulp.watch('src/*.html', gulp.series('html')).on('change', browserSync.reload);
+    gulp.watch('src/css/**/*.css', gulp.series('css')).on('change', browserSync.reload);
+    gulp.watch('src/img/**/*', gulp.series('img')).on('change', browserSync.reload);
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
 gulp.task('default', gulp.parallel(
     'html',
     'css',
-    'img'
+    'img'),
+    gulp.parallel(
+    'serve',
+    'watch'
 ));

cc95e7194ed36e1c8a4e118255961b1e22a4dac9
IvanVitiuk <vitukivan05@gmail.com>
2020-06-08 11:29:57 +0300

    add

--- source/src/js/index.js
+++ source/src/js/index.js
@@ -6,3 +6,21 @@ function MoreDetails(){
     document.title = 'This is more details'
 }
 
+function NewModul(){
+    var id = 'but'
+    var refresh = '<div id="but"><input class="newtextinput" type="text"><input type="button" onclick="NewTextModul()" value="Join Us"></div>';
+    var obj=document.getElementById(id);
+    var o=document.createElement('div');
+    o.innerHTML= refresh;
+    obj.innerHTML=o.children[0].innerHTML;
+
+}
+
+function NewTextModul(){
+    var id = 'but'
+    var refresh = '<div class="but"><div class="subbuttext"> Thanks for your join</div></div>';
+    var obj=document.getElementById(id);
+    var o=document.createElement('div');
+    o.innerHTML= refresh;
+    obj.innerHTML=o.children[0].innerHTML;
+}
